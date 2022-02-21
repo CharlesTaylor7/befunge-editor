51581440b59b2e2bad8fec0f6111deffd6491d59
Charles Taylor <charlestaylor95@gmail.com>
2022-02-20 19:29:25 -0500

    work

--- components/Befunge.tsx
+++ components/Befunge.tsx
@@ -1,3 +1,6 @@
+import { List } from 'immutable'
+import { useState } from 'react'
+
 type Props = {
   height: number
   width: number
@@ -8,8 +11,14 @@ Befunge.defaultProps = {
   width: 5,
 }
 
+function toKey(i: number, j: number, height: number): number {
+  return i + j * height
+}
+
 export default function Befunge(props: Props) {
   const { height, width } = props
+  const [grid, setGrid] = useState<List<string>>(List())
+
   return (
     <div className="w-screen h-screen flex items-center justify-center">
       <table className="table-fixed border-separate">
@@ -17,9 +26,12 @@ export default function Befunge(props: Props) {
           <tr>
             {Array.from({ length: width }, (_, i) => (
               <td className="border text-center text-ellipsis p-0 w-[30px] h-[30px]">
-                <input className="block w-full h-full text-center" 
-                  type="text" 
-                maxLength={1}/>
+                <input
+                  className="block w-full h-full text-center"
+                  type="text"
+                  maxLength={1}
+                  onChange={(e) => setGrid(grid.set(toKey(i, j, height), e.target.value))}
+                />
               </td>
             ))}
           </tr>
