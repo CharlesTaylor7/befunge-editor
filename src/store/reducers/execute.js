48f8a659afb47620d82b97cccef2ddbf31119734
Charles Taylor <charlestaylor95@gmail.com>
2023-07-05 13:13:42 -0400

    Extend test suite; rename interpret to execute

--- src/store/reducers/execute.js
+++ src/store/reducers/execute.js
@@ -47,7 +47,7 @@ export default (
       return R.set(R.lensProp('heading'), 'Left', state);
     case '^':
       return R.set(R.lensProp('heading'), 'Up', state);
-    case 'V':
+    case 'v':
       return R.set(R.lensProp('heading'), 'Down', state);
     case '?':
       return R.set(
@@ -83,7 +83,7 @@ export default (
         state
       );
     case '$':
-      return R.set(
+      return R.over(
         R.lensProp('stack'),
         stack => stack.tail,
         state
@@ -127,9 +127,9 @@ export default (
         R.set(R.lensProp('stack'), rest)
       )(state);
     case '&':
-      return R.set(R.lensProp('pendingUserInput'), 'Number', state);
+      return R.set(R.lensProp('pendingInput'), 'Number', state);
     case '~':
-      return R.set(R.lensProp('pendingUserInput'), 'Character', state);
+      return R.set(R.lensProp('pendingInput'), 'Character', state);
     case '@':
       return R.set(R.lensProp('executionComplete'), true, state);
     case ' ':
