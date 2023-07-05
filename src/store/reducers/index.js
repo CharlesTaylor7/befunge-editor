3dbc8435d7ec234999cb2d794666ed66fe1f46ac
Charles Taylor <charlestaylor95@gmail.com>
2023-07-05 13:08:41 -0400

    Revert "Rest of "CRA - broke testsuite""
    This reverts commit bf6e598fd36a64fc3ce3727f68284082dcb9e95b.

--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -1,8 +1,7 @@
 import * as R from 'ramda'
-import execute from './execute';
+import interpret from './interpret';
 import move from '../../utilities/move';
 import { gridLens } from '../lenses';
-import Stack from '../../utilities/stack';
 
 export default (state, action) => {
 
@@ -39,17 +38,7 @@ export default (state, action) => {
       )(state);
     }
     case "EXECUTE": {
-      return execute(state);
-    }
-    case "PUSH_INPUT": {
-      const { input } = action;
-      if (typeof input !== 'number') {
-        throw new Error('Input must be a number')
-      }
-      return R.pipe(
-        R.set(R.lensProp('pendingInput'), false),
-        R.over(R.lensProp('stack'), Stack.push(input))
-      )(state);
+      return interpret(state);
     }
     default:
       return state;
