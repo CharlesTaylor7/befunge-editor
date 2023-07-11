3bf101d993cd0397816299cab3bc5ccce3d86b50
Charles Taylor <charlestaylor95@gmail.com>
2023-07-11 11:11:05 -0400

    38% code coverage baseline

--- jest.config.js
+++ jest.config.js
@@ -1,34 +1,26 @@
 const { pathsToModuleNameMapper } = require('ts-jest')
-
-// In the following statement, replace `./tsconfig` with the path to your `tsconfig` file
-// which contains the path mapping (ie the `compilerOptions.paths` option):
 const { compilerOptions } = require('./tsconfig')
 
 module.exports = {
   testEnvironment: 'jsdom',
-  // The root of your source code, typically /src
-  // `<rootDir>` is a token Jest substitutes
+  collectCoverageFrom: [
+    'cra/**/*.{js,jsx,ts,tsx}',
+    'components/**/*.{js,jsx,ts,tsx}',
+    'pages/**/*.{js,jsx,ts,tsx}',
+    'utils/**/*.{js,jsx,ts,tsx}',
+  ],
   roots: ['<rootDir>'],
-  // Jest transformations -- this adds support for TypeScript
-  // using ts-jest
   transform: {
     '^.+\\.(js|jsx|ts|tsx)$': ['babel-jest', { presets: ['next/babel'] }],
   },
 
-  // Runs special logic, such as cleaning up components
-  // when using React Testing Library and adds special
-  // extended assertions to Jest
   setupFilesAfterEnv: [
     //"@testing-library/react/cleanup-after-each",
     '@testing-library/jest-dom/extend-expect',
   ],
 
-  // Test spec file resolution pattern
-  // Matches parent folder `__tests__` and filename
-  // should contain `test` or `spec`.
   testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.[t|j]sx?$',
 
-  // Module file extensions for importing
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
 
   moduleNameMapper: {
