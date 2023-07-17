4d5edb68793b028fddadf770b5e303ddc7d087a1
Charles Taylor <charlestaylor95@gmail.com>
2023-07-17 16:33:08 -0400

    Updates

--- static/0AF6UDWGjS0brWqFnzFhZ/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/3b9d1622-8e0f9c54191523a1.js","static/chunks/970-456d2cf25cdb43bf.js","static/chunks/pages/index-ac5af8dc165d9ac9.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
