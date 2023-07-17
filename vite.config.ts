e36c0b9df50162d4117c66d9229bbb4791e766b7
Charles Taylor <charlestaylor95@gmail.com>
2023-07-17 17:30:34 -0400

    Convert from Next.js to Vite (#25)
    * Init vite project
    
    * Set vite base property
    
    * Update config to allow for aliased paths
    
    * Tailwind
    
    * Copy code into vite project
    
    * Fix title; remove svg
    
    * Copy over deps
    
    * TODO
    
    * Copy over package.json scripts
    
    * Merge vite project back to toplevel
    
    * Cleanup
    
    * Test suite still works

---
+++ vite.config.ts
@@ -0,0 +1,15 @@
+import path from 'path'
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: '/befunge-editor/',
+  resolve: {
+    alias: {
+      '@/styles': path.resolve(__dirname, './styles'),
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+})
