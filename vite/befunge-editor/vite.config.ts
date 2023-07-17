6e35effa60fa3fdc2064c935c9cd373331a31a24
Charles Taylor <charlestaylor95@gmail.com>
2023-07-17 16:51:29 -0400

    Update config to allow for aliased paths

--- vite/befunge-editor/vite.config.ts
+++ vite/befunge-editor/vite.config.ts
@@ -1,3 +1,4 @@
+import path from 'path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
@@ -5,4 +6,10 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
   base: '/befunge-editor/',
+  resolve: {
+    alias: {
+      '@/styles': path.resolve(__dirname, './styles'),
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
 })
