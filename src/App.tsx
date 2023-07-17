808bb6f711bb87a95d2615c6e6b2d0e1bdd2347d
Charles Taylor <charlestaylor95@gmail.com>
2023-07-17 19:26:17 -0400

    Quine

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,8 @@ export default function App() {
     <>
       <Befunge
         initialState={{
-          ...gridInit(['&>:1-:v v *_$.@', ' ^    _$>\\:^']),
+          //...gridInit(['&>:1-:v v *_$.@', ' ^    _$>\\:^']),
+          ...gridInit(['01->1# +# :# 0# g# ,# :# 5# 8# *# 4# +# -# _@']),
         }}
       />
     </>
