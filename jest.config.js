6455f4df51fe5372463d2a845ecae0e1d271fe9c
Charles Taylor <charlestaylor95@gmail.com>
2024-04-05 11:42:15 -0400

    Simplify jest

--- jest.config.js
+++ jest.config.js
@@ -2,22 +2,10 @@ const { pathsToModuleNameMapper } = require('ts-jest')
 const { compilerOptions } = require('./tsconfig')
 
 module.exports = {
-  collectCoverageFrom: ['src/**/*.{js,jsx,ts,tsx}'],
+  collectCoverageFrom: ['src/**/*.{js,ts}'],
   roots: ['<rootDir>'],
-  transform: {
-    '^.+\\.(t|j)sx?$': ['@swc/jest'],
-  },
-  setupFilesAfterEnv: [
-    //"@testing-library/react/cleanup-after-each",
-    '@testing-library/jest-dom/extend-expect',
-  ],
-
-  testRegex: 'test\\.[t|j]sx?$',
-
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json'],
-
-  moduleNameMapper: {
-    '\\.(css|less|sass|scss)$': '<rootDir>/__mocks__/styleMock.js',
-    ...pathsToModuleNameMapper(compilerOptions.paths, { prefix: '<rootDir>/' }),
-  },
+  transform: { '^.+\\.(t|j)s$': ['@swc/jest'] },
+  testRegex: 'test\\.[t|j]s$',
+  moduleFileExtensions: ['ts', 'js'],
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, { prefix: '<rootDir>/' }),
 }
